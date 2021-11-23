a87f4368ea773311b712eeed3244241c019c0faa
sumin <kim.sumin.wat2014@gmail.com>
2021-11-23 15:16:31 -0800

    passing data via props

--- src/App.js
+++ src/App.js
@@ -25,10 +25,26 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenseitem>{expenses[0].title}</Expenseitem>
-      <Expenseitem></Expenseitem>
-      <Expenseitem></Expenseitem>
-      <Expenseitem></Expenseitem>
+      <Expenseitem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></Expenseitem>
+      <Expenseitem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></Expenseitem>
+      <Expenseitem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></Expenseitem>
+      <Expenseitem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></Expenseitem>
     </div>
   );
 }
--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,15 +1,12 @@
 import "./Expenseitem.css";
 
-function Expenseitem() {
-  const expenseDate = new Date(2021, 7, 8);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 294.67;
+function Expenseitem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
