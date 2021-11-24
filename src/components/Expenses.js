bb4acc850d671bc5ee91ffb24d917142e643e6a7
sumin <kim.sumin.wat2014@gmail.com>
2021-11-23 17:15:31 -0800

    changed attribute name expenses to expensesItems

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -6,24 +6,24 @@ function Expenses(props) {
   return (
     <div className="expenses">
       <Expenseitem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
+        title={props.expensesItems[0].title}
+        amount={props.expensesItems[0].amount}
+        date={props.expensesItems[0].date}
       ></Expenseitem>
       <Expenseitem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
+        title={props.expensesItems[1].title}
+        amount={props.expensesItems[1].amount}
+        date={props.expensesItems[1].date}
       ></Expenseitem>
       <Expenseitem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
+        title={props.expensesItems[2].title}
+        amount={props.expensesItems[2].amount}
+        date={props.expensesItems[2].date}
       ></Expenseitem>
       <Expenseitem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
+        title={props.expensesItems[3].title}
+        amount={props.expensesItems[3].amount}
+        date={props.expensesItems[3].date}
       ></Expenseitem>
     </div>
   );
