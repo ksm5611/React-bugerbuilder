249bbc9e71949974a9695427acd83814404a6e01
sumin <kim.sumin.wat2014@gmail.com>
2021-11-30 14:33:16 -0800

    created useState for updating title

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
+
 const ExpenseForm = () => {
+  const [fillUpTitle, setFillUpTitle] = useState("");
+
+  const titleChangeHandler = (event) => {
+    setFillUpTitle(event.target.value);
+    console.log(fillUpTitle);
+  };
+
+  const amountChangeHadler = (event) => {
+    console.log(event);
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="num" min="0.01" step="0.01" />
+          <input
+            type="num"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHadler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
