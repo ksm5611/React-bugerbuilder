73dda245847fb666448b747faf62cfd9b4ea6a74
sumin <kim.sumin.wat2014@gmail.com>
2021-11-30 15:35:16 -0800

    updated user input and made the form clear when it is already updated

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,26 +6,64 @@ const ExpenseForm = () => {
   const [changeAmount, setChangeAmount] = useState("");
   const [updateDate, setUpdateDate] = useState("");
 
+  // const [userInput, setUserInput] = useState({
+  //   fillUpTitle: "",
+  //   changeAmount: "",
+  //   updateDate: "",
+  // });
+
   const titleChangeHandler = (event) => {
     setFillUpTitle(event.target.value);
-    console.log(fillUpTitle);
+    // setUserInput((prevState) => {
+    //   return { ...prevState, fillUpTitle: event.target.value };
+    // });
+    // setUserInput({
+    //   ...userInput,
+    //   fillUpTitle: event.target.value,
+    // });
   };
 
   const amountChangeHandler = (event) => {
     setChangeAmount(event.target.value);
-    console.log(changeAmount);
+    // setUserInput({
+    //   ...userInput,
+    //   changeAmount: event.target.value,
+    // });
   };
 
   const dateChangeHandler = (event) => {
     setUpdateDate(event.target.value);
-    console.log(updateDate);
+    // setUserInput({
+    //   ...userInput,
+    //   updateDate: event.target.value,
+    // });
   };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    console.log("event", event);
+
+    const expenseData = {
+      title: fillUpTitle,
+      amount: changeAmount,
+      date: new Date(updateDate),
+    };
+    console.log(expenseData);
+    setFillUpTitle("");
+    setChangeAmount("");
+    setUpdateDate("");
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={fillUpTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -33,6 +71,7 @@ const ExpenseForm = () => {
             type="num"
             min="0.01"
             step="0.01"
+            value={changeAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -42,6 +81,7 @@ const ExpenseForm = () => {
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={updateDate}
             onChange={dateChangeHandler}
           />
         </div>
